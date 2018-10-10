305856ebfe0ef89e0160aa1df9957d8d42a5b653
Dinara Faizullina <imfd@protonmail.com>
2018-10-10 23:41:57 +0300

    Made t-tests and chi-squared tests

--- project/script.R
+++ project/script.R
@@ -8,10 +8,21 @@ summary(absense)
 
 # groups and percentage
 tbl <- table(absense$Reason.for.absence)
-tbl_percent <- cbind(tbl, prop.table(tbl) * 100)
+tbl_p <- cbind(tbl, prop.table(tbl) * 100)
 
 # printing the group tables
 library(xtable)
 data(tbl_percent)
 xtable(head(tbl_percent, 14))
 xtable(tail(tbl_percent, 14))
+
+# t-tests for smokers and drinkers
+t.test(absense$Absenteeism.time.in.hours ~ absense$Social.drinker, alternative="two.sided", conf.level = 0.95)
+t.test(absense$Absenteeism.time.in.hours ~ absense$Social.smoker, alternative="two.sided", conf.level = 0.95)
+t.test(absense$Absenteeism.time.in.hours ~ absense$Disciplinary.failure, alternative="two.sided", conf.level = 0.95)
+
+# contingency table and chi-squared test
+chisq.test(table(absense$Reason.for.absence, absense$Day.of.the.week))
+chisq.test(table(absense$Month.of.absence, absense$Day.of.the.week))
+chisq.test(table(absense$Day.of.the.week, absense$Seasons))
+chisq.test(table(absense$Day.of.the.week, absense$Education))
