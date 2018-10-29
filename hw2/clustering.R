0c38496af88fbc84dfcd4204fa5f8112c21ba42b
Dinara Faizullina <imfd@protonmail.com>
2018-10-29 23:39:38 +0300

    Improved clustering analysis

--- hw2/clustering.R
+++ hw2/clustering.R
@@ -1,29 +1,29 @@
 # get protein data
 install.packages("ClustOfVar")
+
 d <- data(package = "ClustOfVar")
 nm <- d$results[, "Item"]
 data(list = nm, package = "ClustOfVar")
 
-# Hierarchical Clustering
-# normalization is not needed since the data is in gramms
-
-# remove counrty column
-# protein <- protein[, -c(1, 1)]
+# hierarchical clustering
 
 # calculate euclidian distance 
 distance <- dist(protein)
 
-# cluster dendogram with complete linkage
-hc.c <- hclust(distance)
-plot(hc.c, hang = -1)
-plot(hc.c)
+# cluster dendogram
 
-# cluster dendogram with average linkage
+# hc.s <- hclust(distance, method = "ward.D")
+# hc.s <- hclust(distance, method = "ward.D2")
+# hc.s <- hclust(distance, method = "single")
+# hc.s <- hclust(distance, method = "average")
+# hc.s <- hclust(distance, method = "mcquitty")
+# hc.s <- hclust(distance, method = "centroid")
+
+hc.c <- hclust(distance, method = "complete")
 hc.a <- hclust(distance, method = "average")
+
+plot(hc.c, hang = -1)
 plot(hc.a, hang = -1)
-plot(hc.a)
-
-
 
 # determine the number of clusters from the scree plot
 k.max <- 10
@@ -46,36 +46,33 @@ table(member.c, member.a)
 
 # compare cluster means
 # we can see which variables play important role in cluster analysis (with bigger variation)
-aggregate(protein, list(member.c), mean)
+means <- aggregate(protein, list(member.c), mean)
+# print table
+library(xtable)
+xtable(means)
 
 #silhouette plot to see how far are the samples from their cluster
 # we can see Italy is an outlier in cluster 1
 library(cluster)
-plot(silhouette(cutree(hc.c,k.count), distance))
-
-# hc.s <- hclust(distance, method = "ward.D")
-# hc.s <- hclust(distance, method = "ward.D2")
-# hc.s <- hclust(distance, method = "single")
-# hc.s <- hclust(distance, method = "complete")
-hc.s <- hclust(distance, method = "average")
-# hc.s <- hclust(distance, method = "mcquitty")
-hc.s <- hclust(distance, method = "median")
-# hc.s <- hclust(distance, method = "centroid")
-
-plot(silhouette(cutree(hc.s, 5), distance))
-
-# plot(protein.numeric$White.Meat ~ protein.numeric$Fish, data = protein.numeric, col = fit$cluster, pch = 20)
-# plot(protein.numeric$Eggs ~ protein.numeric$Fish, data = protein.numeric, col = fit$cluster, pch = 20)
-# plot(protein.numeric$Milk ~ protein.numeric$Fish, data = protein.numeric, col = fit$cluster, pch = 20)
-# plot(protein.numeric$Fish ~ protein.numeric$Fish, data = protein.numeric, col = fit$cluster, pch = 20)
-# plot(protein.numeric$Cereals ~ protein.numeric$Fish, data = protein.numeric, col = fit$cluster, pch = 20)
-# plot(protein.numeric$Starchy.Foods ~ protein.numeric$Fish, data = protein.numeric, col = fit$cluster, pch = 20)
-# plot(protein.numeric$Nuts ~ protein.numeric$Fish, data = protein.numeric, col = fit$cluster, pch = 20)
-# plot(protein.numeric$Fruite.veg. ~ protein.numeric$Fish, data = protein.numeric, col = fit$cluster, pch = 20)
-# plot(protein.numeric$ ~ protein.numeric$Fish, data = protein.numeric, col = fit$cluster, pch = 20)
-
+plot(silhouette(cutree(hc.m, k.count), distance))
+plot(silhouette(cutree(hc.c, k.count), distance))
 
 # k-means clustering
-fit <- kmeans(protein, 5)
-fit
+fit <- kmeans(protein, k.count)
+
+# observe the clusters
 plot(protein$White.Meat ~ protein$Fish, data = protein, col = fit$cluster, pch = 20)
+plot(protein$Eggs ~ protein$Fish, data = protein, col = fit$cluster, pch = 20)
+plot(protein$Milk ~ protein$Fish, data = protein, col = fit$cluster, pch = 20)
+plot(protein$Fish ~ protein$Fish, data = protein, col = fit$cluster, pch = 20)
+plot(protein$Cereals ~ protein$Fish, data = protein, col = fit$cluster, pch = 20, main = "Scatter plot for protein data with k-means clustering")
+plot(protein$Starchy.Foods ~ protein$Fish, data = protein, col = fit$cluster, pch = 20)
+plot(protein$Nuts ~ protein$Fish, data = protein, col = fit$cluster, pch = 20)
+plot(protein$Fruite.veg. ~ protein$Fish, data = protein, col = fit$cluster, pch = 20)
+plot(protein$Red.Meat ~ protein$Fish, data = protein, col = fit$cluster, pch = 20)
+
+# means for each cluster
+fit.means <- aggregate(protein, list(fit$cluster), mean)
+# print means table
+library(xtable)
+xtable(fit.means)
